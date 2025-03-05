b72bd03393b8300c702929bb8a68e5540c25c080
mausooq <mauchoomausooq051@gmail.com>
2025-03-05 16:39:10 +0530

    rockPaperScissor

---
+++ dom.js
@@ -0,0 +1,15 @@
+
+
+
+// setTimeout(() => {
+//     document.title = "Js Mausooq";
+//     document.body.style.backgroundColor = "yellow";
+// },1000)
+
+// username = "";
+
+// const greet = document.getElementById('greet');
+
+// greet.textContent += username === "" ? " geust" : username;
+
+
--- script.js
+++
@@ -1,6 +0,0 @@
-let name  = "mausooq";
-
-let letter = [...name].join("-");
-console.log(letter); // ["m", "a", "u", "s", "o", "o", "q"]
-
-
